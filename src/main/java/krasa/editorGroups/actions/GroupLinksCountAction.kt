03d521ff12c4747af43c75ae0807d33aa46ab003
Elior <heliosaian@gmail.com>
2025-10-05 21:09:32 +0300

    ✨feat: add scaling, antialiasing and inset fix for tabs and icons

--- src/main/java/krasa/editorGroups/actions/GroupLinksCountAction.kt
+++ src/main/java/krasa/editorGroups/actions/GroupLinksCountAction.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.ui.JBColor
+import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.ui.UIUtil
 import com.intellij.util.ui.UIUtil.drawCenteredString
 import krasa.editorGroups.EditorGroupManager
@@ -23,6 +24,7 @@ class GroupLinksCountAction : DumbAwareAction() {
     val count = currentGroup.size(project)
 
     e.presentation.icon = CompositeIcon(count)
+    e.presentation.text = count.toString()
   }
 
   internal class CompositeIcon(private val counter: Int) : Icon {
@@ -32,13 +34,15 @@ class GroupLinksCountAction : DumbAwareAction() {
     override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
       val counterText = counter.toString()
       val g2d = g as Graphics2D
+      g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+      g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
       val fontSize = if (counter < 100) FONT_SIZE else SMALL_FONT_SIZE
 
       g2d.color = backgroundColor
       g2d.fillOval(x, y, ICON_SIZE, ICON_SIZE)
 
       g2d.color = UIUtil.getPanelBackground()
-      g2d.font = g2d.font.deriveFont(fontSize)
+      g2d.font = g2d.font.deriveFont(fontSize.toFloat())
       drawCenteredString(
         g2d,
         Rectangle(x, y, ICON_SIZE, ICON_SIZE),
@@ -52,8 +56,8 @@ class GroupLinksCountAction : DumbAwareAction() {
   }
 
   companion object {
-    private const val FONT_SIZE = 10.0f
-    private const val SMALL_FONT_SIZE = 8.0f
-    private const val ICON_SIZE = 16
+    private val FONT_SIZE = JBUIScale.scale(10)
+    private val SMALL_FONT_SIZE = JBUIScale.scale(8)
+    private val ICON_SIZE = JBUIScale.scale(16)
   }
 }
